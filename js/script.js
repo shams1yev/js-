065190528ce436f59a8f7ab112ade5b7fc283900
Your Name <you@example.com>
2024-01-18 18:36:19 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let age = confirm(" yaxshi korgan mevangiz bormi?");
+
+if (age == true){
+    let isAge = prompt(" nima nomi")
+    console.log(isAge);
+}
+else { 
+    let isMeva = prompt("nimaga")
+    console.log(isMeva);
+}
